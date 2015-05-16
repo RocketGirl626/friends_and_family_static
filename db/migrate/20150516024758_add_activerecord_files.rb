486bbcca4ee173b7805f75cd061316430ebf2f99
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-05-15 20:09:18 -0700

    create migration file.

--- db/migrate/20150516024758_add_activerecord_files.rb
+++ db/migrate/20150516024758_add_activerecord_files.rb
@@ -1,5 +1,29 @@
 class AddActiverecordFiles < ActiveRecord::Migration
   def change
-  	
+  	 create_table :recipes do |t|
+      t.string :recipe_name
+      t.timestamps null: false
+    end
+
+    create_table :categories do |t|
+      t.string :cat_name
+      t.timestamps null: false
+    end
+
+    create_table :categories_recipes, id: false do |t|
+      t.belongs_to :recipe, index: true
+      t.belongs_to :category, index: true
+    end
+
+    create_table :ingredients do |t|
+    	t.string :ingredient_name
+    	t.integer :recipe_id
+    	t.timestamps null: false
+    end
+
+    create_table :instructions do |t|
+    	t.string :instruction_name
+    	t.integer :recipe_id
+    	t.timestamps null: false
   end
 end
