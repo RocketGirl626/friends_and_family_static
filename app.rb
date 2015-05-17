f9c797942e4ef9adc6233305235f04cf4482c71b
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-05-16 18:20:00 -0700

    added first rspec test and index screen to add recipe header.

--- app.rb
+++ app.rb
@@ -9,51 +9,12 @@ get('/') do
   erb(:index)
 end
 
-get('/recipe/:id') do
-  @recipe = Recipe.find(params.fetch('id'))
-
-  @categories = Category.all()
-  erb(:recipe)
-end
-
-post('/recipe_cat_add/') do
-  cat_name = params.fetch('cat_add')
-  # new_category = Category.create({:cat_name => cat_name})
-  category = Category.find_by({:cat_name => cat_name})
-  recipe_id = params.fetch('recipe_id')
-  recipe = Recipe.find(recipe_id)
-  recipe.categories.push(category)
-  @recipe = recipe
-  erb(:recipe)
-end
-
-get('/category/:id') do
-  @category = Category.find(params.fetch('id'))
-  erb(:category)
-end
-
-
-post('/category_add/') do
-  @cat_name = params.fetch('cat_name')
-  Category.create({:cat_name => @cat_name})
-  @categories = Category.all()
+post('/recipe_add') do
+  @recipe_name = params.fetch('recipe_name')
+  @source = params.fetch('source')
+  @servings = params.fetch('servings')
+  @comment = params.fetch('comment')
+  @recipe = Recipe.create({:recipe_name => @recipe_name, :source => @source, :servings => @servings, :comment => @comment})
   @recipes = Recipe.all()
   erb(:index)
-end
-
-post('/recipe_add/') do
-  @title = params.fetch('title')
-  @rating = params.fetch('rating')
-  @recipe = Recipe.create({:title => @title, :rating => @rating})
-  @categories = Category.all()
-  @recipes = Recipe.all()
-  erb(:index)
-end
-
-post('/ingredient_add/') do
-  @recipe = Recipe.find(params.fetch('id'))
-  @name = params.fetch('name')
-  Ingredient.create({:name => @name, :recipe_id => recipe.id().to_i})
-  @ingredients = Ingredient.all()
-  erb(:recipe)
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,10 +1,10 @@
 require('spec_helper')
 
 describe(Recipe) do
-  describe('#title') do
-    it('will return the title of the recipe') do
-      test_recipe = Recipe.new({:title => 'Classic Lemon Curd', :source => 'Fine Dining, April 2009', :servings => 'Makes 2 cups', :comment => 'blah blah blah', :id => nil})
-      expect(test_recipe.title()).to(eq('Classic Lemon Curd'))
+  describe('#recipe_name') do
+    it('will return the name of the recipe') do
+      test_recipe = Recipe.new({:recipe_name => 'Classic Lemon Curd', :source => 'Fine Dining, April 2009', :servings => 'Makes 2 cups', :comment => 'blah blah blah', :id => nil})
+      expect(test_recipe.recipe_name()).to(eq('Classic Lemon Curd'))
     end
   end
 end
